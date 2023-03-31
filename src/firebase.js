dbccb3cfb36544d469f12a940f6291221e596838
Rimjhim20 <kumaririmjhim720@gmail.com>
2023-03-31 12:01:49 +0530

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,22 +4,20 @@ import 'firebase/messaging';
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-const firebaseConfig = {
-  apiKey: "AIzaSyDhD_RiDejHYsDyXeGZ6Vtiu-KSlq6GFpQ",
-  authDomain: "solution-f5982.firebaseapp.com",
-  databaseURL: "https://solution-f5982-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "solution-f5982",
-  storageBucket: "solution-f5982.appspot.com",
-  messagingSenderId: "123087560437",
-  appId: "1:123087560437:web:7af189c5e721dd67516fbc",
-  measurementId: "G-W0RNFHZVKD"
-};
 
+const app = initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+});
 
-const app = initializeApp(firebaseConfig);
 export const messaging = getMessaging(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 export const database = getDatabase(app);
-
